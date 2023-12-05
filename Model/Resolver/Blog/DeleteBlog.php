5c5c82f77501754437badfa9c646eed4b332028c
MinhPhucNguyen <2051063667@e.tlu.edu.vn>
2023-12-05 17:01:20 +0700

    Update and Delete Blog, Category in GraphQL

---
+++ Model/Resolver/Blog/DeleteBlog.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * @author    Tigren Solutions <info@tigren.com>
+ * @copyright Copyright (c) 2023 Tigren Solutions <https://www.tigren.com>. All rights reserved.
+ * @license   Open Software License ("OSL") v. 3.0
+ *
+ */
+
+namespace Tigren\SimpleBlog\Model\Resolver\Blog;
+
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Throwable;
+use Tigren\SimpleBlog\Model\BlogFactory;
+
+class DeleteBlog implements ResolverInterface
+{
+    protected $blogFactory;
+
+    public function __construct(
+        BlogFactory $blogFactory
+    )
+    {
+        $this->blogFactory = $blogFactory;
+    }
+
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        if (!isset($args['blog_id'])) {
+            throw new GraphQlInputException(__('Required parameter "blog_id" is missing'));
+        }
+
+        $blogId = $args['blog_id'];
+
+        $blog = $this->blogFactory->create();
+
+        try {
+            $blog->load($blogId);
+            $blog->delete();
+            return [
+                'message' => __('The blog has been deleted.')
+            ];
+        } catch (Throwable $e) {
+            throw new GraphQlInputException(__($e->getMessage()));
+        }
+    }
+}
+
---
+++ Model/Resolver/Category/DeleteCategory.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * @author    Tigren Solutions <info@tigren.com>
+ * @copyright Copyright (c) 2023 Tigren Solutions <https://www.tigren.com>. All rights reserved.
+ * @license   Open Software License ("OSL") v. 3.0
+ *
+ */
+
+namespace Tigren\SimpleBlog\Model\Resolver\Category;
+
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Throwable;
+use Tigren\SimpleBlog\Model\CategoryFactory;
+
+class DeleteCategory implements ResolverInterface
+{
+    protected $categoryFactory;
+
+    public function __construct(
+        CategoryFactory $categoryFactory
+    )
+    {
+        $this->categoryFactory = $categoryFactory;
+    }
+
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        if (!isset($args['category_id'])) {
+            throw new GraphQlInputException(__('Required parameter "category_id" is missing'));
+        }
+
+        $categoryId = $args['category_id'];
+
+        $category = $this->categoryFactory->create();
+
+        try {
+            $category->load($categoryId);
+            $category->delete();
+            return [
+                'message' => __('The category has been deleted.')
+            ];
+        } catch (Throwable $e) {
+            throw new GraphQlInputException(__($e->getMessage()));
+        }
+    }
+}
+
